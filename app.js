bfe50b879a25172adab547788c182358c1285821
hernandezedwyn369-png <hernandezedwyn369@gmail.com>
2026-06-14 17:07:48 -0600

    Frontend tienda de ropa

---
+++ app.js
@@ -0,0 +1,51 @@
+const API_URL = "http://localhost:3000/prendas";
+
+async function guardarPrenda(){
+
+const prenda={
+nombre:document.getElementById('nombre').value,
+categoria:document.getElementById('categoria').value,
+talla:document.getElementById('talla').value,
+precio:Number(document.getElementById('precio').value),
+stock:Number(document.getElementById('stock').value),
+imagen:document.getElementById('imagen').value
+};
+
+await fetch(API_URL,{
+method:'POST',
+headers:{
+'Content-Type':'application/json'
+},
+body:JSON.stringify(prenda)
+});
+
+cargarPrendas();
+}
+
+async function cargarPrendas(){
+
+const respuesta=await fetch(API_URL);
+const prendas=await respuesta.json();
+
+const catalogo=document.getElementById('catalogo');
+
+catalogo.innerHTML='';
+
+prendas.forEach(prenda=>{
+
+catalogo.innerHTML += `
+
+<div class="card">
+<img src="${prenda.imagen}">
+<h3>${prenda.nombre}</h3>
+<p>${prenda.categoria}</p>
+<p>Talla: ${prenda.talla}</p>
+<p>$${prenda.precio}</p>
+<p>Stock: ${prenda.stock}</p>
+</div>
+`;
+
+});
+}
+
+cargarPrendas();
